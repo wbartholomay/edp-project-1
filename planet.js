e8c39a9b04739c5e3de8e5e88cfaa12251163cdf
Bartholomay <alice@webage.com>
2024-06-24 12:00:17 -0400

    added character display

--- planet.js
+++ planet.js
@@ -1,10 +1,12 @@
 const planetsList = document.getElementById('planetsList');
 const sp = new URLSearchParams(window.location.search);
 const id = sp.get('id');
+let characters = [];
+const charactersList = document.querySelector("#charactersList")
 
 
 async function getPlanets(){
-  const res = await fetch('https://swapi2.azurewebsites.net/api/planets/${id}')
+  const res = await fetch(`https://swapi2.azurewebsites.net/api/planets/${id}`)
     .then(data => res.json())
     .then(console.log(data))
     .catch(err => {
@@ -13,4 +15,32 @@ async function getPlanets(){
   console.log(data);
 }
 
+async function getCharacters() {
+    let url = 'https://swapi2.azurewebsites.net/api/characters';
+  
+    try {
+      const fetchedCharacters = await fetch(url)
+        .then(res => res.json())
+      characters.push(...fetchedCharacters);
+    }
+    catch (ex) {
+      console.error("Error reading characters.", ex.message);
+    }
+    console.log("All the characters are ", characters)
+    renderCharacters(characters);
+  }
+
+const renderCharacters = characters => {
+    const divs = characters.map(character => {
+      const el = document.createElement('div');
+      el.addEventListener('click', () => goToCharacterPage(character.id));
+      el.textContent = character.name;
+      return el;
+    })
+    charactersList.replaceChildren(...divs)
+  }
+
+const goToCharacterPage = id => window.location = `/character.html?id=${id}`
+
 getPlanets();
+getCharacters();
