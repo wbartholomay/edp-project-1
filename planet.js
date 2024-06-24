d19824f834b39ddb13253026f0a335318269f7a3
Zach Florian <zachary.florian@uconn.edu>
2024-06-24 11:30:43 -0400

    planet.js work

---
+++ planet.js
@@ -0,0 +1,16 @@
+const planetsList = document.getElementById('planetsList');
+const sp = new URLSearchParams(window.location.search);
+const id = sp.get('id');
+
+
+async function getPlanets(){
+  const res = await fetch('https://swapi2.azurewebsites.net/api/films/${id}/planets')
+    .then(data => res.json())
+    .then(console.log(data))
+    .catch(err => {
+      console.log('Error fetching API: ',err);
+    }); 
+  console.log(data);
+}
+
+getPlanets();
