8a7102b8766c692556fadbb13f47b9120b07e536
Evan Phillips <ejp468@gmail.com>
2024-06-24 14:20:18 -0400

    update films html and js

--- film.js
+++ film.js
@@ -2,7 +2,9 @@ const FilmsList = document.getElementById("filmsList");
 const sp = new URLSearchParams(window.location.search);
 const id = sp.get("id");
 let characters = [];
+let planet = [];
 const charactersList = document.querySelector("#charactersList");
+const planetList = document.querySelector("#planetList");
 
 async function getFilms() {
   const res = await fetch(`https://swapi2.azurewebsites.net/api/films/${id}`)
@@ -20,11 +22,11 @@ function fillFields(res) {
   document.getElementById("episode_id").textContent = res["episode_id"];
   document.getElementById("director").textContent = res["director"];
   document.getElementById("release_date").textContent = res["release_date"];
-  document.getElementById("OpeningCrawl").textContent = res["OpeningCrawl"];
+  document.getElementById("OpeningCrawl").textContent = res["opening_crawl"];
 }
 
 async function getCharacters() {
-  let url = "https://swapi2.azurewebsites.net/api/characters";
+  let url = `https://swapi2.azurewebsites.net/api/films/${id}/characters`;
 
   try {
     const fetchedCharacters = await fetch(url).then((res) => res.json());
@@ -34,6 +36,19 @@ async function getCharacters() {
   }
   console.log("All the characters are ", characters);
   renderCharacters(characters);
+}
+
+async function getPlanets() {
+  let url = `https://swapi2.azurewebsites.net/api/films/${id}/planets`;
+
+  try {
+    const fetchedPlanets = await fetch(url).then((res) => res.json());
+    planet.push(...fetchedPlanets);
+  } catch (ex) {
+    planet.error("Error reading characters.", ex.message);
+  }
+  console.log("All the characters are ", planet);
+  renderCharacters(planet);
 }
 
 const renderCharacters = (characters) => {
@@ -46,8 +61,19 @@ const renderCharacters = (characters) => {
   charactersList.replaceChildren(...divs);
 };
 
+const renderPlanets = (planet) => {
+  const divs = planet.map((planet) => {
+    const el = document.createElement("div");
+    el.addEventListener("click", () => (window.location = `/planet?id=${id}`));
+    el.textContent = planet.name;
+    return el;
+  });
+  planetList.replaceChildren(...divs);
+};
+
 const goToCharacterPage = (id) => (window.location = `/character?id=${id}`);
 
 // 4bd794c593689c941b92196e4b7ff19a1410fa89
 getFilms();
 getCharacters();
+getPlanets();
