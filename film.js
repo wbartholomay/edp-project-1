169df17e39917b78aa17304b4116c18722b606ab
Evan Phillips <ejp468@gmail.com>
2024-06-24 13:50:59 -0400

    updated films.js

--- film.js
+++ film.js
@@ -1,6 +1,8 @@
 const FilmsList = document.getElementById("filmsList");
 const sp = new URLSearchParams(window.location.search);
 const id = sp.get("id");
+let characters = [];
+const charactersList = document.querySelector("#charactersList");
 
 async function getFilms() {
   const res = await fetch(`https://swapi2.azurewebsites.net/api/films/${id}`)
@@ -9,6 +11,43 @@ async function getFilms() {
       console.log("Error fetching API: ", err);
     });
   console.log(res);
+  fillFields(res);
 }
 
+function fillFields(res) {
+  document.getElementById("producer").textContent = res["producer"];
+  document.getElementById("title").textContent = res["title"];
+  document.getElementById("episode_id").textContent = res["episode_id"];
+  document.getElementById("director").textContent = res["director"];
+  document.getElementById("release_date").textContent = res["release_date"];
+  document.getElementById("OpeningCrawl").textContent = res["OpeningCrawl"];
+}
+
+async function getCharacters() {
+  let url = "https://swapi2.azurewebsites.net/api/characters";
+
+  try {
+    const fetchedCharacters = await fetch(url).then((res) => res.json());
+    characters.push(...fetchedCharacters);
+  } catch (ex) {
+    console.error("Error reading characters.", ex.message);
+  }
+  console.log("All the characters are ", characters);
+  renderCharacters(characters);
+}
+
+const renderCharacters = (characters) => {
+  const divs = characters.map((character) => {
+    const el = document.createElement("div");
+    el.addEventListener("click", () => goToCharacterPage(character.id));
+    el.textContent = character.name;
+    return el;
+  });
+  charactersList.replaceChildren(...divs);
+};
+
+const goToCharacterPage = (id) => (window.location = `/character?id=${id}`);
+
+// 4bd794c593689c941b92196e4b7ff19a1410fa89
 getFilms();
+getCharacters();
--- planet.js
+++ planet.js
@@ -1,57 +1,55 @@
-const planetsList = document.getElementById('planetsList');
+const planetsList = document.getElementById("planetsList");
 const sp = new URLSearchParams(window.location.search);
-const id = sp.get('id');
+const id = sp.get("id");
 let characters = [];
-const charactersList = document.querySelector("#charactersList")
+const charactersList = document.querySelector("#charactersList");
 
-
-async function getPlanets(){
+async function getPlanets() {
   const res = await fetch(`https://swapi2.azurewebsites.net/api/planets/${id}`)
-    .then(res => res.json())
-    .catch(err => {
-      console.log('Error fetching API: ',err);
-    }); 
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log("Error fetching API: ", err);
+    });
   console.log(res);
   fillFields(res);
 }
 
-function fillFields(res){
-  document.getElementById('climate').textContent = res['climate'];
-  document.getElementById('surface_water').textContent = res['surface_water'];
-  document.getElementById('name').textContent = res['name'];
-  document.getElementById('diameter').textContent = res['diameter'];
-  document.getElementById('rotation_period').textContent = res['rotation_period'];
-  document.getElementById('gravity').textContent = res['gravity'];
-  document.getElementById('orbital_period').textContent = res['orbital_period'];
-  document.getElementById('population').textContent = res['population'];
+function fillFields(res) {
+  document.getElementById("climate").textContent = res["climate"];
+  document.getElementById("surface_water").textContent = res["surface_water"];
+  document.getElementById("name").textContent = res["name"];
+  document.getElementById("diameter").textContent = res["diameter"];
+  document.getElementById("rotation_period").textContent =
+    res["rotation_period"];
+  document.getElementById("gravity").textContent = res["gravity"];
+  document.getElementById("orbital_period").textContent = res["orbital_period"];
+  document.getElementById("population").textContent = res["population"];
 }
 
 async function getCharacters() {
-    let url = 'https://swapi2.azurewebsites.net/api/characters';
-  
-    try {
-      const fetchedCharacters = await fetch(url)
-        .then(res => res.json())
-      characters.push(...fetchedCharacters);
-    }
-    catch (ex) {
-      console.error("Error reading characters.", ex.message);
-    }
-    console.log("All the characters are ", characters)
-    renderCharacters(characters);
-  }
+  let url = "https://swapi2.azurewebsites.net/api/characters";
 
-const renderCharacters = characters => {
-    const divs = characters.map(character => {
-      const el = document.createElement('div');
-      el.addEventListener('click', () => goToCharacterPage(character.id));
-      el.textContent = character.name;
-      return el;
-    })
-    charactersList.replaceChildren(...divs)
+  try {
+    const fetchedCharacters = await fetch(url).then((res) => res.json());
+    characters.push(...fetchedCharacters);
+  } catch (ex) {
+    console.error("Error reading characters.", ex.message);
   }
+  console.log("All the characters are ", characters);
+  renderCharacters(characters);
+}
 
-const goToCharacterPage = id => window.location = `/character?id=${id}`
+const renderCharacters = (characters) => {
+  const divs = characters.map((character) => {
+    const el = document.createElement("div");
+    el.addEventListener("click", () => goToCharacterPage(character.id));
+    el.textContent = character.name;
+    return el;
+  });
+  charactersList.replaceChildren(...divs);
+};
+
+const goToCharacterPage = (id) => (window.location = `/character?id=${id}`);
 
 // 4bd794c593689c941b92196e4b7ff19a1410fa89
 getPlanets();
